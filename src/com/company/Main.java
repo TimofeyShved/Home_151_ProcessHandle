f158975eafa9ebbd8b328cab82689de254bccbb9
TimofeyShved <timofeya123@inbox.ru>
2023-12-17 01:18:05 +0600

    ProcessHandle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // запустили программу в консоли (сон на 1h)
+        Process process = Runtime.getRuntime().exec("cmd sleep 1h");
+
+        System.out.println("У текущей программы id "+ProcessHandle.current().pid());
+        System.out.println("У консольной id "+process.pid());
+
+        //Получим чужие процессы в нашей программе
+        ProcessHandle processHandle = ProcessHandle.of(process.pid()).orElseThrow(IllegalAccessError::new);
+
+        // А дальше добавим дополнительные действия, например при закрытии программы, сообщить
+        processHandle.onExit().thenRun(()-> System.out.println("Консоль закрылась"));
+        // так же узнать пользователя
+        System.out.println(processHandle.info().user().orElse("no user"));
+        // командная строка видна?
+        System.out.println(processHandle.info().commandLine().orElse("Нет"));
+        // уничтожить процесс
+        processHandle.destroy();
+
+        Thread.sleep(100);
+
     }
 }
